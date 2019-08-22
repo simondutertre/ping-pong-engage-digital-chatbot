6e648c79ee0286d8e4965e4329cf7416d455fe8b
Simon.D <simon@lajavaness.com>
2019-08-22 10:57:09 +0200

    add logic to fork

---
+++ src/server/directions.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {name: 'Alsace Franche-Comté', code: 'AL-FRCOMTE', departments: ['25', '39', '70', '67', '68', '90']},
+  {
+    name: 'Champagne-Ardenne', region: 'ARDENNE', code: 'CHAMP-ARDN', departments: ['08', '10', '51', '52']
+  },
+  {name: 'Lorraine', code: 'LORRAINE', departments: ['54', '55', '57', '88']},
+  {name: 'Île-de-France Est', code: 'IDF-EST', departments: ['91', '77', '93', '94']},
+  {name: 'Île-de-France Ouest', code: 'IDF-OUEST', departments: ['78', '92', '95']},
+  {name: 'Paris', code: 'PARIS', departments: ['75']},
+  {name: 'Nord-Pas-de-Calais', code: 'NORD-PDC', departments: ['59', '62']},
+  {name: 'Normandie', code: 'NORMANDIE', departments: ['14', '27', '50', '61', '76']},
+  {name: 'Picardie', code: 'PICARDIE', departments: ['02', '60', '80']},
+  {name: 'Alpes', code: 'ALPES', departments: ['38', '73', '74']},
+  {name: 'Bourgogne', code: 'BOURGOGNE', departments: ['58', '21', '71', '89']},
+  {name: 'Sillon Rhodanien', code: 'SIL-RHODANIEN', departments: ['01', '07', '26', '42', '69']},
+  {name: "Côte d'Azur", code: 'COTEDAZUR', departments: ['06', '83']},
+  {name: 'Languedoc-Roussillon', code: 'LANGUEDOC-ROUSS', departments: ['11', '30', '34', '66']},
+  {name: 'Provence-Alpes du Sud', code: 'PACA', departments: ['04', '05', '13', '84']},
+  {name: 'Aquitaine Nord', code: 'AQUITAINE', departments: ['24', '33', '47']},
+  {name: 'Midi-Pyrénées Sud', code: 'MIDI-PYR-SUD', departments: ['09', '31', '32']},
+  {name: 'Midi-Pyrénées Nord', code: 'MIDI-PYR-NORD', departments: ['12', '46', '48', '81', '82']},
+  {name: 'Pyrénées et Landes', code: 'PYR-LANDES', departments: ['40', '65', '64']},
+  {name: 'Bretagne', code: 'BRETAGNE', departments: ['22', '29', '56', '35']},
+  {name: 'Pays de la Loire', code: 'PAYSDELALOIRE', departments: ['44', '49', '53', '72', '85']},
+  {name: 'Poitou-Charentes', code: 'POITOU-CHAR', departments: ['16', '17', '79', '86']},
+  {name: 'Auvergne', code: 'AUVERGNE', departments: ['03', '15', '43', '63']},
+  {name: 'Centre-Val de Loire', code: 'CENTRE-VALDELOIRE', departments: ['18', '28', '36', '37', '45', '41']},
+  {name: 'Limousin', code: 'LIMOUSIN', departments: ['19', '23', '87']}
+];
--- src/server/index.js
+++ src/server/index.js
@@ -1,42 +1,116 @@
-/**
- * exmaple ping-pong bot file
- * reply to ping with pong
- */
-
-const cheerio = require('cheerio')
 const _ = require('lodash')
 
-exports.name = 'ping-pong bot'
-
-exports.description = 'Simple example engage digital chatbot that will repond with "pong" to message "ping"'
-
-exports.homepage = 'https://github.com/zxdong262/ping-pong-engage-digital-chatbot'
-
-function isPing (event) {
-  let $ = cheerio.load(_.get(event, 'resource.metadata.body'))
-  let txt = $('body').text().trim()
-  console.log('txt:', txt)
-  return txt.includes('ping')
-}
+const enedisDirections = require('./directions')
 
 function isSelf (event) {
   return _.get(event, 'resource.metadata.status') === 'user_initiated'
 }
+
+
+
+const geoCategoryId = '5d516bb3dbddbb5ebc9a128e'
+const rcId = '5d5c170d526722572d05d846'
+const commId = '5d5c1abf526722572d05d973'
+const linkyId = '5d5c1c2e52672239b7a49d67'
+const directionParisId = '5d5d2fb90e69dc3d1f222818'
+const twitterAccountId = '1156582247794974720'
+
+// match first occurence of french postal code in a string
+const regPostalCode = /[0-9]{5}/
+const ML_URL = 'https://3yyqqih8mh.execute-api.eu-central-1.amazonaws.com/default/enedisMain'
+
+const addDirection = obj => {
+  const postalCode = getPostalCode(obj.body)
+
+  if (!postalCode) return obj
+
+  const departmentCode = postalCode.slice(0, 2)
+  const enedisDirection = enedisDirections.find(direction => direction.departments.includes(departmentCode))
+
+  if (!enedisDirection) return obj
+
+  return {
+    ...obj,
+    enedisDirection
+  }
+}
+
+const format = event => ({
+  content_thread_id: "",
+  messages: []
+})
+
+const getPostalCode = string => {
+  const result = regPostalCode.exec(string)
+
+  return result ? result[0] : null
+}
+
+const publicTweetRes = id => `Bonjour, afin que nous puissions traiter au mieux votre demande, merci de nous communiquer votre code postal (5 chiffres) en message privé.
+https://twitter.com/messages/compose?recipient_id=${id}`
+
+const dmTweetRes = () => 'Bonjour, afin que nous puissions traiter au mieux votre demande, merci de nous communiquer votre code postal (5 chiffres).'
+
+
+
 
 exports.onEvent = async ({
   event,
   client,
   handled // hanlded by prev skills
 }) => {
-  if (handled) {
-    return
-  }
-  console.log(event)
-  if (!isSelf(event) && isPing(event)) {
-    let res = {
-      body: 'pong'
+  if (handled) return
+  if (isSelf(event)) return
+
+  console.log(JSON.stringify(event, null, 3))
+  try {
+    // Get all contents by event thread id ( last 30 - default limit )
+    const { data: { records } } = await client.get(`/1.0/contents?q=thread:"${event.resource.metadata.thread_id}" order:created_at.desc`)
+
+    // Reverse() them, because we receive them from most recent, and ML takes it from latest
+    const { data: mlResponse } = await axios.post(ML_URL, records.reverse())
+    const { value: shouldReply } = mlResponse.entities.find(entity => entity.key === 'ShouldReply')
+    const postalCode = getPostalCode(event.resource.metadata.body)
+
+    if (event.resource.metadata.body.includes('@enedis_paris')) {
+      await client.put(`/1.0/contents/${event.resource.id}/update_categories?category_ids[]=${directionParisId}`)
     }
-    await client.reply(event, res)
-    return true
+    // else if(postalCode) {
+    //   await client.put(`/1.0/contents/${event.resource.id}/update_categories?category_ids[]=${directionParisId}`)
+    // }
+
+    if(!shouldReply) return
+
+    const { value: motif } = mlResponse.entities.find(entity => entity.key === 'Motif')
+
+    switch (motif.slice(0, 2)) {
+      case 'RA' || 'RB':
+        await client.put(`/1.0/contents/${event.resource.id}/update_categories?category_ids[]=${rcId}`)
+        break
+      case 'CA' || 'CB':
+        await client.put(`/1.0/contents/${event.resource.id}/update_categories?category_ids[]=${commId}`)
+        break
+      case 'LI01':
+        await client.put(`/1.0/contents/${event.resource.id}/update_categories?category_ids[]=${linkyId}`)
+        break
+    }
+
+    switch (event.resource.type) {
+      case 'twtr/tweet':
+        return await client.reply(event, {
+          body: publicTweetRes(twitterAccountId)
+        })
+      case 'twtr/private_tweet':
+        return await client.reply(event, {
+          body: dmTweetRes()
+        })
+
+      default: return await client.reply(event, {
+        body: 'Bonjour'
+      })
+    }
+
+  } catch(err) {
+    console.log("Caught Error : ", err)
   }
 }
